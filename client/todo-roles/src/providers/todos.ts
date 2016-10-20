ea8b10c403071c04eafcababb03f4e7254e5bcd5
Diego Maia <diegocmsantos@gmail.com>
2016-10-20 11:02:10 -0300

    Project finished

--- client/todo-roles/src/providers/todos.ts
+++ client/todo-roles/src/providers/todos.ts
@@ -1,18 +1,67 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
+import { Auth } from './auth';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the Todos provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class Todos {
 
-  constructor(public http: Http) {
+  constructor(public http: Http, public authService: Auth) {
     console.log('Hello Todos Provider');
+  }
+
+  getTodos() {
+
+  	return new Promise((resolve, reject) => {
+
+  		let headers = new Headers();
+      	headers.append('Authorization', this.authService.token);
+
+  		this.http.get('http://localhost:8080/api/todos', {headers: headers})
+  			.map(res => res.json())
+  			.subscribe(data => {
+  				resolve(data);
+  			}, err => {
+  				reject(err);
+  			});
+
+  	});
+
+  }
+
+  createTodo(todo) {
+
+	return new Promise((resolve, reject) => {
+	  	let headers = new Headers();
+	  	headers.append('Content-Type', 'application/json');
+	  	headers.append('Authorization', this.authService.token);
+
+		this.http.post('http://localhost:8080/api/todos', JSON.stringify(todo), {headers: headers})
+			.map(res => res.json())
+			.subscribe(data => {
+				resolve(data)
+			}, err => {
+				reject(err)
+			});
+	});
+
+  }
+
+  deleteTodo(id){
+ 
+    return new Promise((resolve, reject) => {
+ 
+        let headers = new Headers();
+        headers.append('Authorization', this.authService.token);
+ 
+        this.http.delete('http://localhost:8080/api/todos' + id, {headers: headers}).subscribe((res) => {
+            resolve(res);
+        }, (err) => {
+            reject(err);
+        });    
+ 
+    });
+ 
   }
 
 }
